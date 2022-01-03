b93c47602c3ce7441aa7e66a35c03f15e471cdd7
nickelodeon222 <sandiwch34@gmail.com>
2022-01-03 17:17:10 +1300

    added saves

--- gameCode.js
+++ gameCode.js
@@ -1,14 +1,3 @@
-const bananaCounter = document.getElementById("bananaCount");
-const feedButton = document.getElementById("feedMinion");
-const minionSong = new Howl({
-src: ['minion.mp3']
-});
-const vectorSound = new Howl({
-    src: ['ah-curse-you-tiny-toilet.mp3']
-})
-
-let bananaAmount = 0;
-
 // Increases the number of bananas by one when the "Feed minion" button is clicked
 
 feedButton.addEventListener('click', () => {
@@ -17,54 +6,8 @@ feedButton.addEventListener('click', () => {
     bananaCounter.innerHTML = `YOU HAVE ${Math.floor(bananaAmount)} BANANAS`;
 });
 
-/**
- * Class that represents an item in the game
- */
-class GameItem {
-    amount = 0;
-
-    /**
-     * The amount of bananas you will have after each second with this item
-     * @param {number} bananaAmount 
-     */
-    profit() {
-        return this.amount * this.bananaPerSecond
-    }
-
-    /**
-     * Creates a new item
-     * @param {number} startingCost The base price of your item 
-     * @param {string} name The name of your item 
-     * @param {number} bps The bananas per second one of this item makes
-     * @param {HTMLElement} button The button that displays the cost and amount of your item
-     */
-    constructor(startingCost, name, bps, button) {
-        this.cost = startingCost;
-        this.name = name;
-        this.bananaPerSecond = bps;
-        this.button = button
-    }
-    
-    /**
-     * Increases the amount of items and the cost (DOESN'T DECREASE YOUR AMOUNT OF BANANAS)
-     * @param {number} amount 
-     */
-    increase() {
-        this.amount++;
-        this.cost = this.cost + (this.cost / 10);
-        this.button.innerHTML = `${this.name}, cost: ${this.cost.toFixed(2)} bananas, amount: ${this.amount}, bps: ${this.bananaPerSecond}`;
-    }
-}
-    
-
-// The button to buy the tree
-const bananaTreeButton = document.getElementById('bananaTreeButton');
-
-// Creates an item called the banana tree with 0.5 bps
-const bananaTree = new GameItem(10, "banana tree", 0.5, bananaTreeButton);
-
 // Buys a tree on click of the buy button
-bananaTreeButton.addEventListener('click', () => {
+bananaTree.button.addEventListener('click', () => {
     if (bananaAmount >= bananaTree.cost) {
         bananaAmount -= bananaTree.cost;
         bananaTree.increase();
@@ -72,13 +15,8 @@ bananaTreeButton.addEventListener('click', () => {
     }
 })
 
-const bananaFarmButton = document.getElementById('bananaFarmButton');
-
-// Creates an item called the banana farm with 2 bps
-const bananaFarm = new GameItem(200, "banana farm", 2, bananaFarmButton);
-
 // Buys a farm on click of the buy button
-bananaFarmButton.addEventListener('click', () => {  ;
+bananaFarm.button.addEventListener('click', () => {  ;
     if (bananaAmount >= bananaFarm.cost) {
         bananaAmount -= bananaFarm.cost;
         bananaFarm.increase()
@@ -86,12 +24,8 @@ bananaFarmButton.addEventListener('click', () => {  ;
     }
 });
 
-const importButton = document.getElementById('importButton');
-
-const importedBananas = new GameItem(1000, "import bananas", 5, importButton);
-
 // Buys imported bananas when pressed
-importButton.addEventListener('click', () => {  
+importedBananas.button.addEventListener('click', () => {  
     if (bananaAmount >= importedBananas.cost) {
         bananaAmount -= importedBananas.cost;
         importedBananas.increase();
@@ -99,12 +33,8 @@ importButton.addEventListener('click', () => {
     }
 })
 
-const labourButton = document.getElementById("labourButton");
-
-const slaveLabour = new GameItem(1000, "minion labour", 10, labourButton);
-
 // Buys a slave on click of the buy button
-labourButton.addEventListener('click', () => {  
+slaveLabour.button.addEventListener('click', () => {  
     if (bananaAmount >= slaveLabour.cost) {
         bananaAmount -= slaveLabour.cost;
         slaveLabour.increase();
@@ -112,12 +42,8 @@ labourButton.addEventListener('click', () => {
     }
 })
 
-const humanButton = document.getElementById('humanButton');
-
-const humanLabour = new GameItem(10000, "human labour", 50, humanButton)
-
 // Buys a human slave on click of the buy button
-humanButton.addEventListener('click', () => {  
+humanLabour.button.addEventListener('click', () => {  
     if (bananaAmount >= humanLabour.cost) {
         bananaAmount -= humanLabour.cost;
         humanLabour.increase();
@@ -125,11 +51,7 @@ humanButton.addEventListener('click', () => {
     }
 })
 
-const vectorButton = document.getElementById('vectorButton');
-
-const killVector = new GameItem(500000, 'kill vector', 100, vectorButton);
-
-vectorButton.addEventListener('click', () => {  
+killVector.button.addEventListener('click', () => {  
     if (bananaAmount >= killVector.cost) {
         bananaAmount -= killVector.cost;
         killVector.increase();
@@ -138,10 +60,19 @@ vectorButton.addEventListener('click', () => {
     }
 })
 
-let eatenBananas = 0;
-let bepsMultiplier = 1.01;
+const saveButton = document.getElementById('saveButton');
 
-const allItems = [bananaTree, bananaFarm, importedBananas, slaveLabour, humanLabour, killVector];
+saveButton.addEventListener("click", () => {
+    save();
+});
+
+const resetButton = document.getElementById('resetButton');
+
+resetButton.addEventListener("click", () => {
+    document.cookie.split(';').forEach(c => {
+        document.cookie = c.trim().split('=')[0] + '=;' + 'expires=Thu, 01 Jan 1970 00:00:00 UTC;';
+      });
+});
 
 // Increases your bananas by your BPS every second
 const loop = setInterval(() => {
@@ -180,3 +111,4 @@ const loop = setInterval(() => {
         } 
     })
 }, 1000);
+
---
+++ pageinit.js
@@ -0,0 +1,132 @@
+const bananaCounter = document.getElementById("bananaCount");
+const feedButton = document.getElementById("feedMinion");
+const minionSong = new Howl({
+src: ['minion.mp3']
+});
+const vectorSound = new Howl({
+    src: ['ah-curse-you-tiny-toilet.mp3']
+})
+let bananaAmount = 0;
+
+/**
+ * Class that represents an item in the game
+ */
+ class GameItem {
+    amount = 0;
+
+    /**
+     * The amount of bananas you will have after each second with this item
+     * @param {number} bananaAmount 
+     */
+    profit() {
+        return this.amount * this.bananaPerSecond
+    }
+
+    /**
+     * Creates a new item
+     * @param {number} startingCost The base price of your item 
+     * @param {string} name The name of your item 
+     * @param {number} bps The bananas per second one of this item makes
+     * @param {HTMLElement} button The button that displays the cost and amount of your item
+     */
+    constructor(startingCost, name, bps, button) {
+        this.cost = startingCost;
+        this.name = name;
+        this.bananaPerSecond = bps;
+        this.button = button
+    }
+    
+    /**
+     * Increases the amount of items and the cost (DOESN'T DECREASE YOUR AMOUNT OF BANANAS)
+     * @param {number} amount 
+     */
+    increase() {
+        this.amount++;
+        this.cost = this.cost + (this.cost / 10);
+        this.button.innerHTML = `${this.name}, cost: ${this.cost.toFixed(2)} bananas, amount: ${this.amount}, bps: ${this.bananaPerSecond}`;
+    }
+}
+    
+
+// The button to buy the tree
+const bananaTreeButton = document.getElementById('bananaTreeButton');
+
+// Creates an item called the banana tree with 0.5 bps
+const bananaTree = new GameItem(10, "banana tree", 0.5, bananaTreeButton);
+
+
+const bananaFarmButton = document.getElementById('bananaFarmButton');
+
+// Creates an item called the banana farm with 2 bps
+const bananaFarm = new GameItem(200, "banana farm", 2, bananaFarmButton);
+
+
+const importButton = document.getElementById('importButton');
+
+const importedBananas = new GameItem(1000, "import bananas", 5, importButton);
+
+
+const labourButton = document.getElementById("labourButton");
+
+const slaveLabour = new GameItem(1000, "minion labour", 10, labourButton);
+
+
+const humanButton = document.getElementById('humanButton');
+
+const humanLabour = new GameItem(10000, "human labour", 50, humanButton);
+
+
+const vectorButton = document.getElementById('vectorButton');
+
+const killVector = new GameItem(500000, 'kill vector', 100, vectorButton);
+
+let eatenBananas = 0;
+let bepsMultiplier = 1.01;
+
+const allItems = [bananaTree, bananaFarm, importedBananas, slaveLabour, humanLabour, killVector];
+
+if (!document.cookie == "") {
+    load();
+}
+
+// code for saving and cookies
+function setCookie(cname, cvalue)  {
+    document.cookie = cname + "=" + cvalue + "; path=/";
+}
+  
+function save() {
+    setCookie('bananas', bananaAmount);
+    allItems.forEach(item => {
+        setCookie(`${item.name}Amount`, item.amount);
+        setCookie(`${item.name}Price`, item.cost);
+    })
+    setCookie('eatenBananas', eatenBananas);
+    setCookie('multiplier', bepsMultiplier);
+}
+
+function load() {
+    bananaAmount = parseFloat(getCookie('bananas'));
+    eatenBananas = parseFloat(getCookie('eatenBananas'));
+    bepsMultiplier = parseFloat(getCookie('multiplier'));
+    allItems.forEach(item => {
+        item.amount = parseInt(getCookie(`${item.name}Amount`));
+        item.cost = parseFloat(getCookie(`${item.name}Price`));
+        item.button.innerHTML = `${item.name}, amount: ${item.amount}, cost: ${item.cost} bananas, bps: ${item.bananaPerSecond}`
+    })
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+    }
+    }
+    return "";
+}
